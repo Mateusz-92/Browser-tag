128f724e81df4108ea58f7a964e4caa1b9b85bde
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-04-01 12:13:35 +0200

    feat: improve api file

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,28 +1,26 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 
 export type Tag = {
   count: number;
   name: string;
 };
 
-export const api = {
+export const httpClient = axios.create({
   baseURL: 'https://api.stackexchange.com/',
-  get: async (path: string, params: object = {}) => {
-    const response: AxiosResponse = await axios({
-      method: 'GET',
-      url: `${api.baseURL}${path}`,
-      params,
-    });
-    return response?.data;
+});
+
+httpClient.interceptors.response.use(
+  function (response) {
+    return response;
   },
-};
+  function (error) {
+    return Promise.reject(error);
+  },
+);
 
 export const fetchTags = async (): Promise<Tag[]> => {
-  try {
-    const tags = await api.get('2.3/tags?pagesize=100&order=desc&sort=popular&site=stackoverflow');
-    return tags.items;
-  } catch (error) {
-    console.error('Error fetching tags:', error);
-    throw error;
-  }
+  const { data } = await httpClient.get(
+    '2.3/tags?pagesize=100&order=desc&sort=popular&site=stackoverflow',
+  );
+  return data.items;
 };
