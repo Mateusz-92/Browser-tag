2ba9b4688ab1df5c35abeb3de711785b2db50b41
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-04-02 13:54:32 +0200

    feat: wrapped with AllTheProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import { Home } from './components/Home/Home';
-import { SortProvider } from './context/SortContext';
-import ErrorBoundary from './errors/ErrorBoundery/ErrorBoundery';
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      gcTime: 60_000,
-    },
-  },
-  queryCache: new QueryCache(),
-});
+import AllTheProviders from './providers/AllTheProviders';
 
 const App = () => {
   return (
     <>
-      <ErrorBoundary>
-        <QueryClientProvider client={queryClient}>
-          <SortProvider>
-            <Home />
-          </SortProvider>
-        </QueryClientProvider>
-      </ErrorBoundary>
+      <AllTheProviders>
+        <Home />
+      </AllTheProviders>
     </>
   );
 };
