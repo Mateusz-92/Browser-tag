f92a68822b0f63d26902fa87d5504da612582279
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-04-02 16:40:11 +0200

    feat: delete interceptors

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -9,15 +9,6 @@ export const httpClient = axios.create({
   baseURL: 'https://api.stackexchange.com/',
 });
 
-httpClient.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    return Promise.reject(error);
-  },
-);
-
 export const fetchTags = async (): Promise<Tag[]> => {
   const { data } = await httpClient.get(
     '2.3/tags?pagesize=100&order=desc&sort=popular&site=stackoverflow',
